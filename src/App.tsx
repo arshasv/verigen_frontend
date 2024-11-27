0949c04b1910c9835ba25d2a5f2c9dc15a42a34a
Adithya <adithyadaspk@opentrends.net>
2024-11-27 15:40:00 +0530

    Set new password change in some features.

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import React from 'react';
 import { CssBaseline } from '@mui/material';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'; 
 import SigninPage from './pages/Signin'; 
